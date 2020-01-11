b86a8530a25d7137c9e3555a694cedb92e27d925
LexxxusPtitsa <55672271+LexxxusPtitsa@users.noreply.github.com>
2020-01-11 14:24:54 +0300

    Add files via upload

---
+++ precache-manifest.ebb37cd281fd52157123aa94d9a7d751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef883bf4dfa0b72c6e2d444bc4026ef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "39dd237c680868129e88",
+    "url": "./static/css/main.2d80a60a.chunk.css"
+  },
+  {
+    "revision": "f230dab6705b45baa1b7",
+    "url": "./static/js/2.8c6d8b6e.chunk.js"
+  },
+  {
+    "revision": "9652715b819d26dcc09aad3261a56a36",
+    "url": "./static/js/2.8c6d8b6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39dd237c680868129e88",
+    "url": "./static/js/main.b8555e6c.chunk.js"
+  },
+  {
+    "revision": "08f6055480cda0fe3ef7",
+    "url": "./static/js/runtime-main.46d23265.js"
+  }
+]);
